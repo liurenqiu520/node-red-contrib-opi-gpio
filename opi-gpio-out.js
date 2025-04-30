1a025806fc6ec0d9457607ee083632cfb112ec6b
liurenqiu520 <liurenqiu520@gmail.com>
2025-04-30 08:55:52 +0800

    仅适配香橙派zero2w

--- opi-gpio-out.js
+++ opi-gpio-out.js
@@ -1,42 +1,46 @@
-var Gpio = require('onoff').Gpio;
+const Gpio = require('onoff').Gpio;
 
-module.exports = function(RED) {
-  "use strict";
-  function opiOutNode(config) {
-      RED.nodes.createNode(this,config);
-      var node = this;
-      node.name = config.name;
-      node.pin = config.pin;
-      node.log("Pin: "+node.pin);
-      node.set = config.set;
-      node.level = config.level;
-      var outpin;
-      
-    // init:
-    if(node.pin !== ''){
-      outpin = new Gpio(node.pin, 'out');
-      outpin.unwatch();
-    }
+module.exports = function (RED) {
+    "use strict";
 
-    if (node.set === true){
-        outpin.write(Number(node.level), function(err) {
-            if (err) node.log("gpio.output.error");
+    function opiOutNode(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+        node.name = config.name;
+        node.pin = config.pin;
+        node.log("Pin: " + node.pin);
+        node.set = config.set;
+        node.level = config.level;
+        let outpin;
+
+        // init:
+        if (node.pin !== '') {
+            outpin = new Gpio(node.pin, 'out');
+            outpin.unwatch();
+        }
+
+        if (node.set === true) {
+            outpin.write(Number(node.level), function (err) {
+                if (err) node.log("gpio.output.error");
+            });
+            node.status({fill: "green", shape: "dot", text: node.level.toString()});
+        }
+
+        this.on('input', function (msg) {
+            outpin.write(Number(msg.payload), function (err) {
+                if (err) node.log("gpio.output.error");
+            });
+            node.status({fill: "green", shape: "dot", text: msg.payload.toString()});
+            if (RED.settings.verbose) {
+                node.log("out: " + msg.payload);
+            }
         });
-        node.status({fill:"green",shape:"dot",text:node.level.toString()});
+
+        this.on('close', function () {
+            outpin.unexport();
+            node.log("close ");
+        })
     }
 
-    this.on('input', function(msg){
-      outpin.write(Number(msg.payload), function(err) {
-        if (err) node.log("gpio.output.error");
-      });
-      node.status({fill:"green",shape:"dot",text:msg.payload.toString()});
-      if (RED.settings.verbose) { node.log("out: "+msg.payload); }
-    });
-
-    this.on('close', function(){
-      outpin.unexport();
-      node.log("close ");
-    })
-  }
-  RED.nodes.registerType("opi_out",opiOutNode);
+    RED.nodes.registerType("opi_out", opiOutNode);
 }
